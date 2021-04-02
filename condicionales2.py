d2ceacd1a12001526e28d2a813f50a91f74f4eaf
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2021-04-02 17:47:20 -0600

    2do ejercicio de condicionales

---
+++ condicionales2.py
@@ -0,0 +1,14 @@
+print("¿Cuánto es 2 + 2?")
+print("1). 3")
+print("2). Pez")
+print("3). 4")
+opcion = int(input("Escoje la opción adecuada "))
+
+if opcion == 1:
+    print("Opción incorrecta")
+elif opcion == 2:
+    print("Opción incorrecta")
+elif opcion == 3:
+    print("Opción correcta")
+else:
+    print("Ingrese una opción válida")
