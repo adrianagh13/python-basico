45bb81e04cf95353f4c923372ff08237a39bb074
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2021-04-02 16:47:42 -0600

    se realizó el programa de conversión de monedas

---
+++ conversor2.py
@@ -0,0 +1,7 @@
+dolares = input('¿Cuantos dólares tienes?: ')
+dolares = float(dolares)
+peso = 20
+pesos = dolares * peso
+pesos = round(pesos, 2)
+pesos = str(pesos)
+print('Tienes: ' + pesos + ' pesos')
