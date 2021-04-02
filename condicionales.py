30623aa2d40f64c21007cf50d1bd3c022147c9f5
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2021-04-02 17:22:23 -0600

    ejercicios de condicionales

---
+++ condicionales.py
@@ -0,0 +1,19 @@
+# edad = int(input("Ingresa tu edad: "))
+# if edad > 17:
+#    print("Eres mayor de edad")
+# else: 
+#    print("Eres menor de edad")
+
+# comentar con python, para comentar varias líneas presionamos ctrl + }
+
+numero = int(input('Escribe un numero: '))
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a 5")
+else:
+    print("Es menor a 5")
+
+#otros comandos de python
+# ctrl + ¿ --> separa 4 espacios
+# ctrl + ' --> quita 4 espacios
